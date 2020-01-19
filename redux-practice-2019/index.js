48e36fd04a4e94488e3027d54f8f8dc5fb6ea2b1
Atul Dhotre <atuldhotre993@gmail.com>
2020-01-20 02:00:00 +0530

    added store logic

--- redux-practice-2019/index.js
+++ redux-practice-2019/index.js
@@ -1,3 +1,7 @@
+const redux = require("redux");
+const createStore = redux.createStore;
+
+
 const BUY_CAKE = "BUY_CAKE";
 
 function buyCake() {
@@ -23,3 +27,11 @@ function buyCake() {
         return state;
     }
   };
+
+  const store = createStore(reducer);
+  console.log("initial state", store.getState());
+  const unsubscribe = store.subscribe(() => console.log("updated state", store.getState()));
+  store.dispatch(buyCake());
+  store.dispatch(buyCake());
+  store.dispatch(buyCake());
+  unsubscribe();
