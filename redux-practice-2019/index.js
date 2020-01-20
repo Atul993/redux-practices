8c5d7917998e034fdbf84ae44c8a461f8eaab07a
Atul Dhotre <atuldhotre993@gmail.com>
2020-01-21 02:00:00 +0530

    added middleware logic

--- redux-practice-2019/index.js
+++ redux-practice-2019/index.js
@@ -1,6 +1,10 @@
 const redux = require("redux");
+const reduxLogger = require("redux-logger");
+
 const createStore = redux.createStore;
 const combineReducers = redux.combineReducers;
+const applyMiddleware = redux.applyMiddleware;
+const logger = reduxLogger.createLogger();
 
 
 const BUY_CAKE = "BUY_CAKE";
@@ -82,7 +86,7 @@ const iceCreamReducer = (state = initialIceCreamState, action) => {
     iceCream: iceCreamReducer,
   });
 
-  const store = createStore(rootReducer);
+  const store = createStore(rootReducer, applyMiddleware(logger));
   console.log("initial state", store.getState());
   const unsubscribe = store.subscribe(() => console.log("updated state", store.getState()));
   store.dispatch(buyCake());
