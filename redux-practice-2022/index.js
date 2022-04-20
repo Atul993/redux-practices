a42edf23eaa195e7393391b8290e48b94be7b958
Atul Dhotre <atuldhotre993@gmail.com>
2022-04-20 21:00:00 +0530

    add reducer logic

--- redux-practice-2022/index.js
+++ redux-practice-2022/index.js
@@ -9,4 +9,19 @@ function orederCake() {
   };
 }
 
+const initialState = {
+  numOfCakes: 10,
+};
 
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case CAKE_ORDERED:
+      return {
+        ...state,
+        numOfCakes: state.numOfCakes - 1,
+      };
+
+    default:
+      return state;
+  }
+};
